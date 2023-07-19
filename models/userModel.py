f1dfe5eb32e7607f8a1cab4c83d10a5bc660ffa9
mahatab <official.mahatab21@gmail.com>
2023-07-19 19:32:10 +0530

    New flask app added

--- models/userModel.py
+++ models/userModel.py
@@ -1,9 +1,17 @@
-from database.db_connection import db
+# from database.db_connection import db
 from bson.timestamp import Timestamp
 import datetime as dt
+from database.db_connection import db
 
 class Users(db.Document):
     fullName = db.StringField(required=True)
     email = db.StringField(required=True)
     password = db.StringField(required=True)
-    timestamp = Timestamp(int(dt.datetime.today().timestamp()), 1)
+    # timestamp = Timestamp(int(dt.datetime.today().timestamp()), 1)
+
+
+# class People(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     fullName = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), nullable=False)
+#     password = db.Column(db.String(120), nullable=False)
